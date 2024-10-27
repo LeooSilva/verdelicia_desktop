881ef86cd5a614990e735b34b55dac9dd1cb5312
LeooSilva <leonardogomessilva1910@gmail.com>
2024-10-26 22:58:52 -0300

    Adiciona HTML e CSS do projeto

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // Inicializa o Electron
+        var host = CreateHostBuilder(args).Build();
+        ElectronBootstrap(); // Inicia o Electron
+        await host.RunAsync();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+
+    public static async void ElectronBootstrap()
+    {
+        // Cria a janela principal do Electron
+        var options = new BrowserWindowOptions
+        {
+            Width = 800,
+            Height = 600
+        };
+
+        var window = await Electron.WindowManager.CreateWindowAsync(options);
+        window.OnClosed += () => Electron.App.Quit();
+    }
+}
